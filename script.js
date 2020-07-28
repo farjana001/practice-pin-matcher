13837490a59d7ab4142ae0ae407937236bbc9620
Farjana Islam <nipa.farzana87@gmailcon>
2020-07-29 00:04:45 +0600

    some css updated

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// var btnOne = document.getElementById('calc-one');
-// var btnTwo = document.getElementById('calc-two');
-// var btnThree = document.getElementById('calc-three');
-// var btnFour = document.getElementById('calc-four');
-// var btnFive = document.getElementById('calc-five');
-// var btnSix = document.getElementById('calc-six');
-// var btnSeven = document.getElementById('calc-seven');
-// var btnEight = document.getElementById('calc-eight');
-// var btnNine = document.getElementById('calc-nine');
-// var btnZero = document.getElementById('calc-zero');
 
-var clearBtn = document.getElementById('calc-clear');
-var calcBackspace = document.getElementById('calc-backspace');
-var displayNumberValue = document.getElementById('calc-input-display');
+// pin generate part
+// var pinGenerate = document.getElementById('pin-generate');
+//     pinGenerate.addEventListener('click', function(){
+//         var pinRandom = Math.random() * 1000;
+//         var pinRandomNum = Math.round(pinRandom);
+//         console.log(pinRandomNum);
+//     })
+
+
+
+
+// calculator part
 
 var displayValue = "";
-var evalStringArray = [];
-
-var calcNumBtn = document.getElementsByClassName('calc-btn');
+var inputDisplay = document.getElementById('calc-input-display');
 
 var updateDisplayValue = (clickObj) => {
     var btnText = clickObj.target.innerText;
@@ -25,16 +22,29 @@ var updateDisplayValue = (clickObj) => {
        displayValue = "";
 
     displayValue += btnText;
-    displayNumberValue.value = displayValue;
+    inputDisplay.value = displayValue;
 }
 
+var calcNumBtn = document.getElementsByClassName('calc-btn');
 for (let i = 0; i < calcNumBtn.length; i++) {
-    calcNumBtn[i].addEventListener('click', updateDisplayValue, false);
+    calcNumBtn[i].addEventListener('click', updateDisplayValue);
     
 }
 
+// clear button
+var clearBtn = document.getElementById('calc-clear');
 clearBtn.onclick = () => {
     displayValue = "";
-    evalStringArray = [];
-    displayNumberValue.value = displayValue;
+    inputDisplay.value = displayValue;
+}
+
+// backspace button
+var backspaceBtn = document.getElementById('calc-backspace');
+backspaceBtn.onclick = () => {
+    let lengthOfDisplayValue = displayValue.length;
+    displayValue = displayValue.slice(0, lengthOfDisplayValue - 1);
+
+    if(displayValue === "")
+       displayValue = "";
+       inputDisplay.value = displayValue;
 }
